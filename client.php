f650393e9e5fd20a504278d46c326fcf1c60a638
andrei.diaconescu <andrei.diaconescu@anais.digital>
2016-10-28 15:49:59 +0300

    more code examples

--- client.php
+++ client.php
@@ -1,102 +1,159 @@
 <?php
-    $sendbirdApplicationId = '38A89BF0-B085-4F76-981D-FC5695FABC1B';
+$sendbirdApplicationId = '38A89BF0-B085-4F76-981D-FC5695FABC1B';
 
-    $userId = 'upcall_usr_1000';
-    $userAccessToken = 'dc4dae9488be14aca328560f51a870c21af0d1dc';
+$userId = 'upcall_usr_1000';
+$userAccessToken = 'dc4dae9488be14aca328560f51a870c21af0d1dc';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Sendbird POC</title>
+<head>
+    <meta charset="utf-8">
+    <title>Sendbird POC</title>
 
-        <style type="text/css">
-            .output_box_bottom {
-                width: 100%;
-                height: 300px;
-                position: absolute;
-                bottom: 0;
-                left: 0;
-                border: 1px solid black;
-                overflow: auto;
+    <style type="text/css">
+        .output_box_bottom {
+            width: 100%;
+            height: 300px;
+            position: absolute;
+            bottom: 0;
+            left: 0;
+            border: 1px solid black;
+            overflow: auto;
+        }
+    </style>
+
+</head>
+<body>
+
+<h3>Sendbird actions</h3>
+
+
+<script>
+    var currentGroupChannel = null;
+    window.addEventListener("load", function (event) {
+        var sb = new SendBird({
+            appId: '<?= $sendbirdApplicationId ?>'
+        });
+
+        var userIdsForGroupChannel = ['upcall_usr_2000', 'upcall_usr_3000'];
+        connectSendbirdUser(sb)
+            .then(function () {
+                return listGroupChannels(sb);
+            })
+            .then(function () {
+                return initGroupChannel(sb, userIdsForGroupChannel);
+            })
+            .then(function () {
+                return sendMessageToGroupChannel(userIdsForGroupChannel, 'test message '+(new Date()).toISOString());
+            })
+        ;
+
+    });
+
+</script>
+
+<script>
+
+    function poc_output(msg) {
+        var elOutputBox = document.querySelector(".output_box_bottom");
+        elOutputBox.innerHTML = elOutputBox.innerHTML + '<br>' + msg;
+    }
+
+
+    function connectSendbirdUser(sb) {
+        var deferredConnectUser = Q.defer();
+        poc_output('+++ Trying to connect user ... ');
+        sb.connect('<?= $userId; ?>', '<?= $userAccessToken; ?>', function (user, error) {
+            poc_output('user: ' + JSON.stringify(user));
+            poc_output('error: ' + JSON.stringify(error));
+
+            if (!error) {
+                deferredConnectUser.resolve(user);
+            } else {
+                deferredConnectUser.reject(error);
             }
-        </style>
+        });
 
-    </head>
-    <body>
+        return deferredConnectUser.promise;
+    }
 
-        <h3>Sendbird actions</h3>
+    function listGroupChannels(sb) {
+        var deferredGroupChannelUsers = Q.defer();
+        poc_output('+++ Trying to get group channels ... ');
+        var channelListQuery = sb.GroupChannel.createMyGroupChannelListQuery();
+        channelListQuery.includeEmpty = true;
 
+        poc_output('channelListQuery.hasNext: ' + JSON.stringify(channelListQuery.hasNext));
+        if (channelListQuery.hasNext) {
+            var resNext = channelListQuery.next(function (channelList, error) {
+                poc_output('channelList: ' + JSON.stringify(channelList));
+                poc_output('error: ' + JSON.stringify(error));
+                poc_output('resNext: ' + JSON.stringify(resNext));
 
-        <script>
-            window.addEventListener("load", function(event) {
-                var sb = new SendBird({
-                    appId: '<?= $sendbirdApplicationId ?>'
-                });
-
-                connectSendbirdUser(sb).then(function() {
-                    listGroupChannels(sb);
-                });
-
-            });
-
-        </script>
-
-        <script>
-
-            function poc_output(msg) {
-                var elOutputBox = document.querySelector(".output_box_bottom");
-                elOutputBox.innerHTML = elOutputBox.innerHTML + '<br>' + msg;
-            }
-
-
-            function connectSendbirdUser(sb) {
-                var deferredConnectUser = Q.defer();
-                poc_output('Trying to connect user ... ');
-                sb.connect('<?= $userId; ?>', '<?= $userAccessToken; ?>', function(user, error) {
-                    poc_output('user: ' + JSON.stringify(user));
-                    poc_output('error: '+ JSON.stringify(error));
-
-                    if (!error) {
-                        deferredConnectUser.resolve(user);
-                    } else {
-                        deferredConnectUser.reject(error);
-                    }
-                });
-
-                return deferredConnectUser.promise;
-            }
-
-            function listGroupChannels(sb) {
-                var deferredGroupChannelUsers = Q.defer();
-                poc_output('Trying to get group channels ... ');
-                var channelListQuery = sb.GroupChannel.createMyGroupChannelListQuery();
-                channelListQuery.includeEmpty = true;
-
-                if (channelListQuery.hasNext) {
-                    channelListQuery.next(function(channelList, error){
-                        poc_output('channelList: ' + JSON.stringify(channelList));
-                        poc_output('error: '+ JSON.stringify(error));
-
-                        if (!error) {
-                            deferredGroupChannelUsers.resolve(channelList);
-                        } else {
-                            deferredGroupChannelUsers.reject(error);
-                        }
-                    });
+                if (!error) {
+                    deferredGroupChannelUsers.resolve(channelList);
                 } else {
-                    poc_output('channelListQuery.hasNext: ' + JSON.stringify(channelListQuery.hasNext));
-                    deferredGroupChannelUsers.reject('channelListQuery.hasNext is false');
+                    deferredGroupChannelUsers.reject(error);
                 }
+            });
+        } else {
+            poc_output('channelListQuery.hasNext: ' + JSON.stringify(channelListQuery.hasNext));
+            deferredGroupChannelUsers.reject('channelListQuery.hasNext is false');
+        }
 
-                return deferredGroupChannelUsers.promise;
-            }
+        return deferredGroupChannelUsers.promise;
+    }
 
-        </script>
-        <script src="/q.js"></script>
-        <script src="/SendBird.min.js"></script>
+    function initGroupChannel(sb, userIds) {
+        var deferredInitGroupChannel = Q.defer();
+        poc_output('+++ Trying to create BY REUSING a group channel, composed of users: ' + userIds.join(','));
 
-        <div class="output_box_bottom"></div>
-    </body>
+        if (currentGroupChannel) {
+            deferredInitGroupChannel.resolve(currentGroupChannel);
+        } else {
+            var groupChannelName = ('Group ' + userIds.join(',')).substr(0, 20) + '...';
+            sb.GroupChannel.createChannelWithUserIds(userIds, true, groupChannelName, 'some_cover_url', 'some_data', function (channel, error) {
+                poc_output('channel: ' + JSON.stringify(channel));
+                poc_output('error: ' + JSON.stringify(error));
+
+                if (!error) {
+                    currentGroupChannel = channel;
+                    deferredInitGroupChannel.resolve(channel);
+                } else {
+                    deferredInitGroupChannel.reject(error);
+                }
+            });
+        }
+
+        return deferredInitGroupChannel.promise;
+    }
+
+    function sendMessageToGroupChannel(userIds, messageToSendToGroupChannel) {
+        var deferredSendMessageToGroupChannel = Q.defer();
+        poc_output('+++ Trying to send message to a group channel, composed of users: ' + userIds.join(','));
+
+        if (!currentGroupChannel) {
+            deferredSendMessageToGroupChannel.reject('Cannot send message, because group channel not initialised, currentGroupChannel');
+        } else {
+            currentGroupChannel.sendUserMessage(messageToSendToGroupChannel, 'some data for msg', function (message, error) {
+                poc_output('message: ' + JSON.stringify(message));
+                poc_output('error: ' + JSON.stringify(error));
+
+                if (!error) {
+                    deferredSendMessageToGroupChannel.resolve(message);
+                } else {
+                    deferredSendMessageToGroupChannel.reject(error);
+                }
+            });
+        }
+        return deferredInitGroupChannel.promise;
+    }
+
+</script>
+<script src="/q.js"></script>
+<script src="/SendBird.min.js"></script>
+
+<div class="output_box_bottom"></div>
+</body>
 </html>
--- server.php
+++ server.php
@@ -5,12 +5,9 @@ $sendbirdApiToken = '51892ab192ab9c98833f496272393b76913a540f';
 $task = isset($_GET['task']) ? $_GET['task'] : null;
 
 switch ($task) {
-    // Create a SendBird user account via Platform API when your user signs up your service.
-    case 'createSendbirdAccount': {
-        $task($sendbirdApiToken);
-        break;
-    }
-    case 'createSendbirdGroupChannel': {
+    case 'createSendbirdAccount':
+    case 'createSendbirdGroupChannel':
+    case 'listSendbirdGroupChannels': {
         $task($sendbirdApiToken);
         break;
     }
@@ -20,6 +17,7 @@ switch ($task) {
 }
 
 /**
+ * Create a SendBird user account via Platform API when your user signs up your service.
  * http://sendbird-poc.me/server.php?task=createSendbirdAccount&user_id=upcall_usr_1000&nickname=nick_upcall_usr_1000&profile_url=url_upcall_usr_1000&issue_access_token=true
  */
 function createSendbirdAccount($sendbirdApiToken)
@@ -57,15 +55,15 @@ function createSendbirdAccount($sendbirdApiToken)
 
 /**
  * http://sendbird-poc.me/server.php?task=createSendbirdGroupChannel&name=upcall_grp_channel_1000&cover_url=upcall_grp_channel_1000_cover_url&data=upcall_grp_channel_1000_data&user_ids=upcall_usr_1000,upcall_usr_2000&is_distinct=true
-*/
+ */
 function createSendbirdGroupChannel($sendbirdApiToken)
 {
     echo '<br>Executing task createSendbirdGroupChannel';
 
-    $name       = $_GET['name'];
-    $coverUrl   = $_GET['cover_url'];
-    $data       = $_GET['data'];
-    $userIds    = $_GET['user_ids'];
+    $name = $_GET['name'];
+    $coverUrl = $_GET['cover_url'];
+    $data = $_GET['data'];
+    $userIds = $_GET['user_ids'];
     $isDistinct = $_GET['is_distinct'];
 
     $sendbirdParams = [
@@ -95,27 +93,49 @@ function createSendbirdGroupChannel($sendbirdApiToken)
     echo '</pre>';
 }
 
+/**
+ * http://sendbird-poc.me/server.php?task=createSendbirdGroupChannel&name=upcall_grp_channel_1000&cover_url=upcall_grp_channel_1000_cover_url&data=upcall_grp_channel_1000_data&user_ids=upcall_usr_1000,upcall_usr_2000&is_distinct=true
+ */
+function listSendbirdGroupChannels($sendbirdApiToken)
+{
+    echo '<br>Executing task listSendbirdGroupChannels';
+
+    $sendbirdParams = [
+        'limit'      => 100,
+        'order'      => 'chronological',
+        'member'     => 'true',
+        'show_empty' => 'true'
+    ];
+
+    $urlQueryString = http_build_query($sendbirdParams);
+    $sendbirdUrl = 'https://api.sendbird.com/v3/group_channels?' . $urlQueryString;
+
+    $sendBirdResponse = curlGet(
+        $sendbirdUrl,
+        [
+            'Content-Type: application/json, charset=utf8',
+            'Api-Token: ' . $sendbirdApiToken
+        ]
+    );
+
+    echo '<br>';
+    echo '$sendBirdResponse: <pre>';
+    var_dump($sendBirdResponse);
+    echo '</pre>';
+}
+
+
 function curlPost($url, $stringPostParams, array $extraHeaders = [], $customPost = false)
 {
     $ch = curl_init();    // initialize curl handle
     curl_setopt($ch, CURLOPT_URL, $url);
-//    curl_setopt($ch, CURLOPT_USERAGENT,
-//        "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/20100101 Firefox/49.0");
-//    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);// allow redirects
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // return into a variable
     curl_setopt($ch, CURLOPT_TIMEOUT, 60); // times out after 60s
-//    curl_setopt($ch, CURLOPT_ENCODING,
-//        ""); // all supported values for http Accept-Encoding header(like "identity", "deflate", and "gzip")
-//    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-//    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-
-//    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
-
     if ($customPost) {
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
     } else {
-        curl_setopt($ch, CURLOPT_POST,
-            1); // set classical POST method, application/x-www-form-urlencoded kind, most commonly used by HTML forms
+        // set classical POST method, application/x-www-form-urlencoded kind, most commonly used by HTML forms
+        curl_setopt($ch, CURLOPT_POST, 1);
     }
 
     curl_setopt($ch, CURLOPT_POSTFIELDS, $stringPostParams); // add POST fields
@@ -128,6 +148,25 @@ function curlPost($url, $stringPostParams, array $extraHeaders = [], $customPost
     $requestResultInfo = curl_getinfo($ch);
     curl_close($ch);
 
+
+    return $responseContent;
+}
+
+function curlGet($url, array $extraHeaders = [])
+{
+    $ch = curl_init();    // initialize curl handle
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // return into a variable
+    curl_setopt($ch, CURLOPT_TIMEOUT, 60); // times out after 60s
+    curl_setopt($ch, CURLOPT_HTTPGET, true);
+
+    if ($extraHeaders) {
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $extraHeaders);
+    }
+
+    $responseContent = curl_exec($ch); // run the whole process
+    $requestResultInfo = curl_getinfo($ch);
+    curl_close($ch);
 
     return $responseContent;
 }
